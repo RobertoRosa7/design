0c85f0a584cb02ab537b3939603de7110657cc65
Roberto Rosa <roberto.rosa7@gmail.com>
2023-03-08 22:23:04 -0300

    update

---
+++ projects/demo/lib/src/lib/lib.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { LibComponent } from './lib.component';
+import { ButtonComponent } from './button/button.component';
+
+
+
+@NgModule({
+  declarations: [
+    LibComponent,
+    ButtonComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    LibComponent,
+    ButtonComponent
+  ]
+})
+export class LibModule { }
---
+++ projects/demo/lib/src/public-api.ts
@@ -0,0 +1,8 @@
+/*
+ * Public API Surface of lib
+ */
+
+export * from './lib/lib.service';
+export * from './lib/lib.component';
+export * from './lib/lib.module';
+export * from './lib/button/button.component';
